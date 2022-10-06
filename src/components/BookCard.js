e77d002b52203a5143d9ad988e3d2200f9258b50
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-06 21:56:08 +0300

    modify header

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,31 +4,39 @@ import Card from 'react-bootstrap/Card';
 
 export default function BookCard({ book, addToRead }) {
     return (
-        <Card className="d-md-flex" >
-            <div className="" key="book" >
-                <div className="image">
-                    <img alt="Snap! We lost an image!" src={book.cover} />
-                </div>
-                <div className="content">
-                    <div className="header">
+        <Card>
+            <div className="container" key="book" >
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card">
+                            <div className="image">
+                                <img alt="Snap! We lost an image!" src={book.cover} />
+                            </div>
+                            <div className="content">
+                                <div className="header">
+                                    <h3>  {book.title} </h3>
                      <h3>  {book.title} </h3> 
-                    </div>
-                    <div className="meta text-wrap">
-                        <h6>{book.author}</h6>
-                    </div>
-                </div>
+                                    <h3>  {book.title} </h3>
+                                </div>
+                                <div className="meta text-wrap">
+                                    <h6>{book.author}</h6>
+                                </div>
+                            </div>
 
-                <div className="extra content">
-                    <span> {book.desc} </span>
+                            <div className="extra content">
+                                <span> {book.desc} </span>
 
-                    <span>
-                        <div className="ui center aligned segment basic">
-                            <Button className="btn btn-outline-light" key={book.id} onClick={() => addToRead(book)} > Add to Reading List </Button>
-                            <span>
-                                <Button className="btn btn-outline-warning" onClick={console.log("click")} >Add to Read</Button>
-                            </span>
+                                <span>
+                                    <div className="ui center aligned segment basic">
+                                        <Button className="btn btn-outline-light" key={book.id} onClick={() => addToRead(book)} > Add to Reading List </Button>
+                                        <span>
+                                            <Button className="btn btn-outline-warning" onClick={console.log("click")} >Add to Read</Button>
+                                        </span>
+                                    </div>
+                                </span>
+                            </div>
                         </div>
-                    </span>
+                    </div>
                 </div>
             </div>
 
