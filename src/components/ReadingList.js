336ba2c80e26525ef311b028f99189c84a2416e4
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-06 15:27:27 +0300

    get reading list from collection

--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import BookCard from './BookCard'
 
-export default function ReadingList() {
+export default function ReadingList({collections}) {
+  const addedBook = collections.map((book) =>{
+    return  <BookCard key={book} book={book} />
+  })
   return (
-    <div>ReadingList</div>
+    <div>
+      {addedBook}
+    </div>
   )
 }
