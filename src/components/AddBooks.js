798cd6c720cc731fb891e643d08d7ccd2661774f
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-06 15:29:32 +0300

    POST data to the backend

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,22 +1,44 @@
 import React from 'react'
-import BookCard from './BookCard'
 
-export default function AddBooks({books}) {
+
+export default function AddBooks({handleAdd }) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newBook = {
+      title: e.target.title.value,
+      author: e.target.author.value,
+      cover: e.target.cover.value,
+      desc: e.target.desc.value,
+    }
+    fetch("http://localhost:3000/books", {
+      method: "POST",
+      headers: {
+        "content-Type":"application/json",
+      },
+      body: JSON.stringify(newBook)
+    })
+    .then((res) => res.json())
+    .then(() => {
+   return handleAdd(newBook)
+  }
+    )
+    handleAdd(newBook)
+    e.target.reset();
+  }
   return (
     <React.Fragment>
-    <nav className="navbar bg-inherit">
-  <div className="container-fluid">
-    <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-      <button className="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
- </nav>
- <div>
-  {books.map((book) =>(
-    <BookCard key={book.id} book={book} />
-  ))}
- </div>
- </React.Fragment>
+      <div className="form-outline">
+        <form className="form-control" onSubmit={handleSubmit} >
+          <div className="input-fields">
+            <input type="text" placeholder="Enter Book Title" name="title"/>
+            <input type="text" placeholder='Author' name="author"/>
+            <input type="url" placeholder="Cover url" name="cover" />
+            <input type='text' placeholder='book description' name="desc "/>
+            <input className="btn btn-primary" type="submit" value='Add Book'/>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
   )
 }
