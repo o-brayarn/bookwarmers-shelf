8a8df8293b7b5cbe0152557a4f87f7689e493c6a
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-06 15:28:21 +0300

    GET data from API mockup and display collection

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import BookCard from './BookCard'
 
-export default function Home() {
+export default function Home({ books, handleSearch, addToRead }) {
   return (
-    <div>Home</div>
+    <>
+      <nav className="navbar bg-inherit">
+        <div className="container-fluid">
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="text" placeholder="Search" aria-label="Search" onChange={(e) => handleSearch(e.target.value)} />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      <div>
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} addToRead={addToRead} />
+        ))}
+      </div>
+    </>
   )
 }
