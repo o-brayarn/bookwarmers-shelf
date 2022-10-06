0bc5b62de94a54096ebbba2ce24eae506bc97d6f
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-06 08:49:02 +0300

    implement the NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import Button from 'react-bootstrap/Button'
+
+export default function NavBar() {
+  return (
+    <Navbar className='navbar' collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand className="brand" href="/">BOOKWARMER'S SHELF</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+
+          <Nav className="me-auto">
+          </Nav>
+
+          <Nav className="navlink">
+              <NavLink to="/reading-list" exact="true"> READING LIST </NavLink>
+              <NavLink to="/read" exact="true"> READ </NavLink>
+              <NavLink to="/add-books" exact="true"><Button variant="outline-info">+ADD</Button></NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
